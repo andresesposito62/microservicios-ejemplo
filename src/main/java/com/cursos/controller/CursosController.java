f0181371fc0aab4ec370005581ca0020cd5cc6c7
andresesposito62 <77408396+andresesposito62@users.noreply.github.com>
2024-01-17 20:19:54 -0500

    put and post methods are added

--- src/main/java/com/cursos/controller/CursosController.java
+++ src/main/java/com/cursos/controller/CursosController.java
@@ -2,9 +2,12 @@ package com.cursos.controller;
 
 import com.cursos.model.Curso;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
@@ -45,6 +48,27 @@ public class CursosController {
             }
         }
         return aux;
+    }
+
+    @DeleteMapping(value = "curso/{name}")
+    public void eliminarCurso(@PathVariable("name") String nombre){
+        cursos.removeIf(c -> c.getNombre().equals(nombre));
+    }
+
+    @PostMapping(value = "curso", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Curso> altaCurso(@RequestBody Curso curso){
+        cursos.add(curso);
+        return cursos;
+    }
+
+    @PutMapping(value = "curso", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Curso> actualizaCurso(@RequestBody Curso curso){
+        for (int i = 0; i <cursos.size(); i++){
+            if (cursos.get(i).getNombre().equals(curso.getNombre())){
+                cursos.set(i, curso);
+            }
+        }
+        return cursos;
     }
 
 }
