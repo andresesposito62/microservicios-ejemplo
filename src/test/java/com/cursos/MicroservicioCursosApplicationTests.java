cc58bd5370991ac39f9e08d5dc795ef0fc39cd3d
andresesposito62 <77408396+andresesposito62@users.noreply.github.com>
2024-01-21 15:56:03 -0500

    MicroservicioCursosApplicationTests is implemented

--- src/test/java/com/cursos/MicroservicioCursosApplicationTests.java
+++ src/test/java/com/cursos/MicroservicioCursosApplicationTests.java
@@ -1,13 +1,24 @@
 package com.cursos;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@AutoConfigureMockMvc
 @SpringBootTest
 class MicroservicioCursosApplicationTests {
 
+	@Autowired
+	MockMvc mock;
+
 	@Test
-	void contextLoads() {
+	void testCursos() throws Exception{
+		mock.perform(get("/cursos")).andDo(print());
 	}
 
 }
